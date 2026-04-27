4c81923b1383657f2ee846fc0daae1ed5944173e
amshuman50 <amshuman789@gmail.com>
2026-04-27 15:16:05 +0545

    stores port number and mongodbUrl from .env file

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,10 @@
 import dotenv from "dotenv";
 
- dotenv.config();
+dotenv.config();
 
- 
+const config = {
+    port: process.env.PORT || 1234,
+    mongodbUrl: process.env.MONGOBD_URL || ""
+};
+
+export default config;
