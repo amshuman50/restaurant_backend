e5e848bb319ede17b92e07f2e8de99afe683bc56
amshuman50 <amshuman789@gmail.com>
2026-04-27 15:16:28 +0545

    Connecting with MongoDB

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import config from "./config.js";
+
+function connectDB(){
+    mongoose.connect(config.mongodbUrl).then(()=>{
+        console.log("MongoDB Connected Successfully.")
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
+
+export default connectDB
