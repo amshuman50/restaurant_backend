888fc05a14ad477c4abd605e34d80a2dfab0a8d2
amshuman50 <amshuman789@gmail.com>
2026-04-27 16:34:38 +0545

    Adding a Semi Colon

--- src/routes/menuItem.route.js
+++ src/routes/menuItem.route.js
@@ -4,7 +4,7 @@ import menuItemController from "../controlers/menuItem.controller.js";
 const router = express.Router();
 
 router.get("/", menuItemController.getAllMenuItems);
-router.get("/:id", menuItemController.getMenuItemById)
+router.get("/:id", menuItemController.getMenuItemById);
 router.post("/", menuItemController.createMenuItem);
 router.put("/:id", menuItemController.updateMenuItem);
 router.delete("/:id", menuItemController.deleteMenuItem);
