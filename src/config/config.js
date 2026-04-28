30bca84dc8122a72ef382b51a6592acf154780a7
amshuman50 <amshuman789@gmail.com>
2026-04-28 17:15:58 +0545

    Added jwt_secret from .env

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,8 @@ dotenv.config();
 
 const config = {
     port: process.env.PORT || 1234,
-    mongodbUrl: process.env.MONGOBD_URL || ""
+    mongodbUrl: process.env.MONGOBD_URL || "",
+    jwtSecret: process.env.JWT_SECRET || ""
 };
 
 export default config;
