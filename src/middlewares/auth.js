b53b31b3857d9b0963cf5a0554ba5da2a5d88faf
amshuman50 <amshuman789@gmail.com>
2026-04-30 12:05:48 +0545

    Middleware for authentication or protecting routes.

---
+++ src/middlewares/auth.js
@@ -0,0 +1,19 @@
+import jwt from "../utils/jwt.js"
+
+const auth = (req, res, next) => {
+    const cookie = req.headers.cookie;
+    if (!cookie) return res.status(401).send("User not authenticated")
+
+    const token = cookie.split("=")[1];
+    if (!token) return res.status(401).send("User not authenticated.")
+
+    try {
+        const data = jwt.verifyToken(token);
+        req.user = data;
+        next();
+    }catch(erorr){
+        res.status(401).send("Invalid Token");
+    }
+};
+
+export default auth;
