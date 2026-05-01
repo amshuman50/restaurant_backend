28c8e91cc3bbfced41ff5dbd65fe45a981133059
amshuman50 <amshuman789@gmail.com>
2026-05-01 10:55:59 +0545

    Using zod validations.

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,12 @@
 //In Postman: localhost:8000/api/auth/register
 import express from "express";
 import authController from "../controlers/auth.controller.js"
+import validate from "../middlewares/validator.js";
+import { loginSchema, registerSchema } from "../libs/schemas/auth.schema.js";
 
 const router = express.Router();
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post("/register", validate(registerSchema), authController.register);
+router.post("/login", validate(loginSchema), authController.login);
 
 export default router;
--- src/routes/menuItem.route.js
+++ src/routes/menuItem.route.js
@@ -3,12 +3,14 @@ import menuItemController from "../controlers/menuItem.controller.js";
 import auth from "../middlewares/auth.js";
 import roleBasedAuth from "../middlewares/roleBasedAuth.js";
 import { ROLE_ADMIN } from "../constants/roles.js"
+import validate from "../middlewares/validator.js";
+import { menuItemSchema } from "../libs/schemas/menuItem.schema.js";
 
 const router = express.Router();
 
 router.get("/", menuItemController.getAllMenuItems);
 router.get("/:id", menuItemController.getMenuItemById);
-router.post("/", auth, roleBasedAuth(ROLE_ADMIN), menuItemController.createMenuItem);
+router.post("/", auth, roleBasedAuth(ROLE_ADMIN), validate(menuItemSchema), menuItemController.createMenuItem);
 router.put("/:id", auth, roleBasedAuth(ROLE_ADMIN), menuItemController.updateMenuItem);
 router.delete("/:id", auth, roleBasedAuth(ROLE_ADMIN), menuItemController.deleteMenuItem);
 
