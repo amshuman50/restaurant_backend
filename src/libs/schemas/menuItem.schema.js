6dbc2cb65404a33cebee0250243d9ec6e5d3220e
amshuman50 <amshuman789@gmail.com>
2026-05-03 12:47:40 +0545

    Price in menuItemSchema is required when there are no any variants.

--- src/libs/schemas/menuItem.schema.js
+++ src/libs/schemas/menuItem.schema.js
@@ -1,9 +1,21 @@
 import z, { boolean, maxLength, minLength } from "zod";
 
-export const menuItemSchema=z.object({
-    name:z.string().check(minLength(1),maxLength(30)),
-    category:z.string(),
-    price:z.number().min(0.01).max(100000),
-    veg:z.boolean().optional(),
+const variantSchema = z.object({
+    name: z.string().check(minLength(1), maxLength(30)),
+    price: z.number().min(0.01).max(100000),
+    veg: z.boolean().optional(),
     isAvailable: boolean().default(true)
 })
+
+export const menuItemSchema = z
+    .object({
+        name: z.string().check(minLength(1), maxLength(30)),
+        category: z.string(),
+        price: z.number().min(0.01).max(100000).optional(),
+        veg: z.boolean().optional(),
+        isAvailable: boolean().default(true),
+        variants: z.array(variantSchema).optional()
+    })
+    .refine((data) => data.price || data.variants, {
+        message: "Item Price is required if there are no Variants",
+    })
